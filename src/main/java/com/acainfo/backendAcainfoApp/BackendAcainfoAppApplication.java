73bd8b4167a3c3d03f4e78f0cf01b57f4004ad52
Pablo Calahorro Jimenez <pcj00004@reed.ujaen.es>
2025-06-29 10:51:27 +0200

    Initial commit: Spring Boot backend setup with PostgreSQL and test profile

---
+++ src/main/java/com/acainfo/backendAcainfoApp/BackendAcainfoAppApplication.java
@@ -0,0 +1,13 @@
+package com.acainfo.backendAcainfoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAcainfoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAcainfoAppApplication.class, args);
+	}
+
+}
